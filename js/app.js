b9e5744a221783cc167e8bde307cfb51782fa12e
ChaimaeChr <123027484+CXaymae@users.noreply.github.com>
2023-05-31 17:38:44 +0200

    feat

--- js/app.js
+++ js/app.js
@@ -28,6 +28,25 @@ function show_all(){
         document.getElementById("js").style.display="block";
         document.getElementById("result").style.display="block";
     }
+    
+    window.addEventListener("DOMContentLoaded", function() {
+    // Check if there is a previously saved HTML code in localStorage
+    if(localStorage.getItem("htmlCode")) {
+        // If there is, retrieve it from localStorage and set the value of the "htmlCode" textarea to the saved code
+        document.getElementById("htmlCode").value = localStorage.getItem("htmlCode");
+    }
+    // Check if there is a previously saved CSS code in localStorage
+    if(localStorage.getItem("cssCode")) {
+        // If there is, retrieve it from localStorage and set the value of the "cssCode" textarea to the saved code
+        document.getElementById("cssCode").value = localStorage.getItem("cssCode");
+    }
+    // Check if there is a previously saved JavaScript code in localStorage
+    if(localStorage.getItem("jsCode")) {
+        // If there is, retrieve it from localStorage and set the value of the "jsCode" textarea to the saved code
+        document.getElementById("jsCode").value = localStorage.getItem("jsCode");
+    }
+    showPreview(); // Call the showPreview function to update the preview after loading the saved code
+});
     if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
     {
         document.getElementById("css").style.display="none";
